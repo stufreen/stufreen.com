9c7e1b1122b7e68e80c002069f829d58fed7b3ee
Stu Freen <stufreen@gmail.com>
2023-08-15 10:39:01 -0400

    Add work list section

---
+++ src/works.js
@@ -0,0 +1,28 @@
+import wdsImage from "./assets/wds1.png";
+import clmImage from "./assets/clm.png";
+import rpcImage from "./assets/rpc.png";
+
+const works = [
+  {
+    title: "Web Drum Sequencer",
+    description:
+      "Browser-based drum machine built with the web audio API and React.",
+    image: wdsImage,
+    url: "https://wds-1.com",
+  },
+  {
+    title: "Cover Letter Magic",
+    description: "AI cover letter generator powered by Chat GPT.",
+    image: clmImage,
+    url: "https://cover-letter-magic.pages.dev",
+  },
+  {
+    title: "Robin Porter Copy",
+    description:
+      "Portfolio site for advertising Copywriter/ACD. Built with Astro and Contentful.",
+    image: rpcImage,
+    url: "https://robinportercopy.ca",
+  },
+];
+
+export default works;
